6c75b86b13545ae611c7d0b26c4f8f61a7f95df7
franfan <13889441200@163.com>
2019-06-04 21:57:02 +0800

    添加对AOP的支持

--- backend/src/main/java/com/fanhehe/cmd/service/HomeService.java
+++ backend/src/main/java/com/fanhehe/cmd/service/HomeService.java
@@ -3,7 +3,6 @@ package com.fanhehe.cmd.service;
 import java.util.Map;
 import java.util.Optional;
 import com.fanhehe.cmd.entity.User;
-import org.aspectj.lang.annotation.Before;
 
 public interface HomeService {
 
--- backend/src/main/java/com/fanhehe/cmd/service/aspect/PictureServiceAspect.java
+++ backend/src/main/java/com/fanhehe/cmd/service/aspect/PictureServiceAspect.java
@@ -1,11 +1,27 @@
 package com.fanhehe.cmd.service.aspect;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
+@Aspect
 class PictureServiceAspect {
 
-    @Pointcut("** com.fanhehe.cmd.service.aspect.PictureServiceAspect.excuete(..)")
-    public void excuete() {}
+    private static final Logger logger = LoggerFactory.getLogger(PictureServiceAspect.class);
 
-    public void writePictureLog() {}
+    @Pointcut("execution(* com.fanhehe.cmd.service.HomeService.handleIndex(..))")
+    public void handleIndex() {}
+
+    @Before("execution(* com.fanhehe.cmd.service.HomeService.handleIndex(..))")
+    public void target() {
+        logger.info("------------ aop executed !!!!!!!!!!");
+    }
+
+    @Before(value="handleIndex()")
+    public void beforeHandleIndex() {
+        logger.info("------------ before execute !!!!!!!!");
+    }
+
 }
